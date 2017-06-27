652843ba3d68fa2dfd5928fd7d16c96316e3cc0e
Yahir Bravo <yahirb@gmail.com>
2017-06-26 22:49:48 -0500

    added click on image to refresh feature, added __init__.py files to two directories, removed initial period from import files in app.py

--- aaas-app/app.py
+++ aaas-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 import requests
 from images.getimage import getImageURL
@@ -8,17 +8,28 @@ from scripts.next_animal import getNextAnimalObject
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    randomName = getRandomName('scripts/random-name-list.txt')
-    animalObject = getImageURL("images/next-animal.txt")
-    nextAnimalObject = getNextAnimalObject("images/next-animal.txt")
-    return render_template('index.html',
-                            randomName = animalObject['nickname'],
-                            animalName = animalObject['name'],
-                            randomNameNext = nextAnimalObject['nickname'],
-                            animalNameNext = nextAnimalObject['name'],
-                            url=animalObject['URL'])
+    if request.method == "POST":
+        randomName = getRandomName('scripts/random-name-list.txt')
+        animalObject = getImageURL("images/next-animal.txt")
+        nextAnimalObject = getNextAnimalObject("images/next-animal.txt")
+        return render_template('index.html',
+                                randomName = animalObject['nickname'],
+                                animalName = animalObject['name'],
+                                randomNameNext = nextAnimalObject['nickname'],
+                                animalNameNext = nextAnimalObject['name'],
+                                url=animalObject['URL'])
+    else:
+        randomName = getRandomName('scripts/random-name-list.txt')
+        animalObject = getImageURL("images/next-animal.txt")
+        nextAnimalObject = getNextAnimalObject("images/next-animal.txt")
+        return render_template('index.html',
+                                randomName = animalObject['nickname'],
+                                animalName = animalObject['name'],
+                                randomNameNext = nextAnimalObject['nickname'],
+                                animalNameNext = nextAnimalObject['name'],
+                                url=animalObject['URL'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug = True)
